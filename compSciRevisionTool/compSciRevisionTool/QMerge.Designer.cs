f78d164cadacff67b49cc17c85472e26436e4766
Hamza Siddique <hamzasiddique7@gmail.com>
2021-10-04 12:19:29 +0100

    Qmerge updates

--- compSciRevisionTool/compSciRevisionTool/QMerge.Designer.cs
+++ compSciRevisionTool/compSciRevisionTool/QMerge.Designer.cs
@@ -29,18 +29,134 @@ namespace compSciRevisionTool
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelQuestion = new System.Windows.Forms.Label();
+            this.labelQuesExplain = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.labelTest = new System.Windows.Forms.Label();
+            this.buttonGenerateQuestion = new System.Windows.Forms.Button();
+            this.buttonSubmitAnswer = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // labelQuestion
+            // 
+            this.labelQuestion.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.labelQuestion.AutoSize = true;
+            this.labelQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelQuestion.Location = new System.Drawing.Point(263, 146);
+            this.labelQuestion.Name = "labelQuestion";
+            this.labelQuestion.Size = new System.Drawing.Size(115, 39);
+            this.labelQuestion.TabIndex = 11;
+            this.labelQuestion.Text = "label1";
+            this.labelQuestion.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // labelQuesExplain
+            // 
+            this.labelQuesExplain.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.labelQuesExplain.AutoSize = true;
+            this.labelQuesExplain.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelQuesExplain.Location = new System.Drawing.Point(263, 93);
+            this.labelQuesExplain.Name = "labelQuesExplain";
+            this.labelQuesExplain.Size = new System.Drawing.Size(561, 38);
+            this.labelQuesExplain.TabIndex = 15;
+            this.labelQuesExplain.Text = "Use merge sort to sort the following:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 15);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Difficulty:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DisplayMember = "1";
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 27);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(137, 21);
+            this.comboBox1.TabIndex = 13;
+            // 
+            // labelTest
+            // 
+            this.labelTest.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelTest.AutoSize = true;
+            this.labelTest.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTest.Location = new System.Drawing.Point(12, 485);
+            this.labelTest.Name = "labelTest";
+            this.labelTest.Size = new System.Drawing.Size(115, 39);
+            this.labelTest.TabIndex = 12;
+            this.labelTest.Text = "label1";
+            // 
+            // buttonGenerateQuestion
+            // 
+            this.buttonGenerateQuestion.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.buttonGenerateQuestion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonGenerateQuestion.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonGenerateQuestion.Location = new System.Drawing.Point(12, 54);
+            this.buttonGenerateQuestion.Name = "buttonGenerateQuestion";
+            this.buttonGenerateQuestion.Size = new System.Drawing.Size(137, 35);
+            this.buttonGenerateQuestion.TabIndex = 10;
+            this.buttonGenerateQuestion.Text = "Generate";
+            this.buttonGenerateQuestion.UseVisualStyleBackColor = false;
+            // 
+            // buttonSubmitAnswer
+            // 
+            this.buttonSubmitAnswer.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.buttonSubmitAnswer.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.buttonSubmitAnswer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.buttonSubmitAnswer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSubmitAnswer.Location = new System.Drawing.Point(270, 280);
+            this.buttonSubmitAnswer.Name = "buttonSubmitAnswer";
+            this.buttonSubmitAnswer.Size = new System.Drawing.Size(137, 35);
+            this.buttonSubmitAnswer.TabIndex = 9;
+            this.buttonSubmitAnswer.Text = "Submit";
+            this.buttonSubmitAnswer.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(270, 239);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(221, 35);
+            this.textBox1.TabIndex = 8;
             // 
             // QMerge
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1013, 535);
+            this.Controls.Add(this.labelQuestion);
+            this.Controls.Add(this.labelQuesExplain);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.labelTest);
+            this.Controls.Add(this.buttonGenerateQuestion);
+            this.Controls.Add(this.buttonSubmitAnswer);
+            this.Controls.Add(this.textBox1);
             this.Name = "QMerge";
             this.Load += new System.EventHandler(this.QMerge_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelQuestion;
+        private System.Windows.Forms.Label labelQuesExplain;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label labelTest;
+        private System.Windows.Forms.Button buttonGenerateQuestion;
+        private System.Windows.Forms.Button buttonSubmitAnswer;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
