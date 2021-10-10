6020487002fc067c20ca2ecfb9547f5a20829cbc
Hamza Siddique <hamzasiddique7@gmail.com>
2021-10-10 15:03:37 +0100

    removed mdf files

--- compSciRevisionTool/compSciRevisionTool/Properties/Settings.Designer.cs
+++ compSciRevisionTool/compSciRevisionTool/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace compSciRevisionTool.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.5.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -31,6 +31,17 @@ namespace compSciRevisionTool.Properties {
         public string revisionDBConnectionString {
             get {
                 return ((string)(this["revisionDBConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"E:\\Hamza Siddique\\Documents\\" +
+            "csRevisionDB.mdf\";Integrated Security=True;Connect Timeout=30")]
+        public string csRevisionDBConnectionString {
+            get {
+                return ((string)(this["csRevisionDBConnectionString"]));
             }
         }
     }
