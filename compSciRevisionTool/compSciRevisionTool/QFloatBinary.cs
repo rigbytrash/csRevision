90aeaa917bdcc2b473117f1844d6841b87bf067a
Hamza Siddique <hamzasiddique7@gmail.com>
2021-11-16 14:11:42 +0000

    fixed point conversion now works...

--- compSciRevisionTool/compSciRevisionTool/QFloatBinary.cs
+++ compSciRevisionTool/compSciRevisionTool/QFloatBinary.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace compSciRevisionTool
 {
@@ -21,8 +22,15 @@ namespace compSciRevisionTool
 
         private void QFloatBinary_Load(object sender, EventArgs e)
         {
-            Form msg3 = new messgaeBox(insertPoint(generateBinary(5)), subColour);
-            Form msg3 = new messgaeBox(insertPoint(generateBinary(3)), subColour);
+            //Form msg3 = new messgaeBox(insertPoint(generateBinary(5)), subColour);
+            ////Form msg2 = new messgaeBox(generateBinary(3), subColour);
+            //Form msg4 = new messgaeBox(insertPoint(generateBinary(5)), subColour);
+
+            string b4 = insertPoint(generateBinary(5));
+            double after = twoscomplementFixedPointBinaryToDouble(b4);
+            Form mss = new messgaeBox(after.ToString(), subColour);
+            Form mdss = new messgaeBox(b4, subColour);
+
         }
 
         private string generateBinary(int bitsWanted)
@@ -48,11 +56,65 @@ namespace compSciRevisionTool
             string exponant = insertPoint(generateBinary(3));
         }
 
-        private int twosComplimentFloaingBinaryToDecimal(string binaryString)
+        private double twoscomplementFixedPointBinaryToDouble(string binaryString)
         {
+            bool hasPoint = false;
+            string afterPoint = "";
             string[] parts = binaryString.Split('.');
+            if (binaryString.Contains("."))
+            {
+                hasPoint = true;
+                afterPoint = parts[1];
+            }
+
             string beforePoint = parts[0];
-            string afterPoint = parts[1];
+            double finalNum = 0;
+
+            char[] beforePointArrayR = utils.reverseLetters(beforePoint).ToCharArray();
+
+            if (hasPoint)
+            {
+                char[] afterPointArray = afterPoint.ToCharArray();
+                for (int i = 0; i < afterPoint.Length; i = i + 1)
+                {
+                    if (afterPointArray[i] == '1')
+                    {
+                        finalNum = finalNum + Math.Pow(2, -(i + 1));
+                    }
+                    Debug.WriteLine("finalNum = " + finalNum.ToString());
+                }
+            }
+
+            for (int i = 0; i < beforePoint.Length; i = i+ 1)
+            {
+                if (i == beforePoint.Length - 1)
+                {
+                    if (beforePointArrayR[i] == '1')
+                    {
+                        finalNum = finalNum - Math.Pow(2,i);
+                    }
+                }
+                else
+                {
+                    if (beforePointArrayR[i] == '1')
+                    {
+                        finalNum = finalNum + Math.Pow(2, i);
+                    }
+                }
+                Debug.WriteLine("finalNum = " + finalNum.ToString());
+            }
+            return finalNum;
+        }
+
+        private double twoscomplementFloatingPointBinaryToDouble(string mantissa, string exponent)
+        {
+            double exponentValue = twoscomplementFixedPointBinaryToDouble(exponent);
+            char[] mantissaArray = mantissa.ToCharArray();
+
+            for (int i = 0; i < exponentValue; i = i + 1)
+            {
+
+            }
 
             return 0;
         }
