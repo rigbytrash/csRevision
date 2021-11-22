8653a274eed5565e6f7eff2b26ebcb4186616342
Hamza Siddique <hamzasiddique7@gmail.com>
2021-11-22 12:25:03 +0000

    start of push

--- compSciRevisionTool/compSciRevisionTool/formDesignForQuestionForms.cs
+++ compSciRevisionTool/compSciRevisionTool/formDesignForQuestionForms.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace compSciRevisionTool
 {
@@ -22,12 +23,35 @@ namespace compSciRevisionTool
 
         }
 
-        private void quesCorrect(string topicName, string username)
+        private string[] quesCorrect(int topicID, int userID) // returns currentDifficulty & conseqCorrect
         {
             // grabs the conseq ques correct for a specific topic
             // adds one or ( sets it to zero and increases the current difficulty, but if the current difficulty is currently the max difficulty, then set COMPLETE to be true)
             // if complete is true:
             //
+
+            // check to see if the entry exists
+            // if so, add 1 to the value / other things
+            // if not, create the first entry
+            SqlConnection Connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=F:\Documents\RevisionStorageDB.mdf;Integrated Security=True;Connect Timeout=30"); // should be made with the declerations but is here to stop errors as the table doesn't exist at the time of programming
+            Connection.Open();
+            string SQLquery = "Select * from UserTopicsTable where userID= '" + userID + "' and topicID= '" + topicID + "'";
+            SqlCommand cmd = new SqlCommand(SQLquery, Connection);
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                dr.Read();
+                int _conseqCorrect = Convert.ToInt32(dr["conseqCorrect"]);
+                if (_conseqCorrect < 4)
+                {
+                    cmd.CommandText = "Update UserTopicsTable SET conseqCorrect= " + _conseqCorrect + 1 + " Where userID= " + userID;
+                }
+                else
+                {
+
+                }
+                int _diff = 0;
+            }
         }
 
         private void grabCurrentDifficulty(string topicName, string username)
